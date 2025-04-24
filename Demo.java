0f7a5cfec73088a47f2ff8f75a98353ae8ef912f
satwik1111 <naikn5637@gmail.com>
2025-04-24 11:30:37 +0530

    save file

---
+++ Demo.java
@@ -0,0 +1,13 @@
+class Demo
+{
+    public static void main(String[] args)
+    {
+        int a=20;
+        int b=20;
+        int sum = a+b;
+        int sub = a-b;
+        System.out.println("Addition is: "+sum);
+        System.out.println("Subtraction is: "+sub);
+        
+    }
+}
